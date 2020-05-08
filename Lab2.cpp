9246f80225655cfcce5582e03c732558de1dcc15
agreyest <56709605+agreyest@users.noreply.github.com>
2020-05-08 14:53:10 -0600

    ejercicio 2 terminado

--- Lab2.cpp
+++ Lab2.cpp
@@ -1,4 +1,4 @@
-#inclue <iostream>
+#include <iostream>
 using namespace std;
 
 int menu(){
@@ -8,7 +8,7 @@ int menu(){
 			<<"1.- Ejercicio 1"<<endl
 			<<"2.- Ejercicio 2"<<endl
 			<<"3.- Ejercicio 3"<<endl
-			<<"4.- Salir"<<endl
+			<<"4.- Salir"<<endl;
 			
 			cin>>retValue;
 			
@@ -37,7 +37,35 @@ int main(){
 			break;
 		}
 		case 2:{
-			
+			int filas;
+			cout<<endl<<"Cuantas filas quiere en el triangulo de pascal: ";
+			cin>>filas;
+			while(filas<1){
+				cout<<endl<<"intentelo de nuevo, el numero debe ser mayor a 0: ";
+				cin>>filas;
+			}
+			int pascal[filas][filas];
+			for(int i = 0; i < filas; i++){
+				if(i == 0){
+					pascal[i][0]=1;	
+				}else if(i == 1){
+					pascal[i][0]=1;
+					pascal[i][1]=1;
+				}else{
+					for(int j = 1; j < i; j++){
+						pascal[i][i]= 1;
+						pascal[i][0]= 1;
+						pascal[i][j]= pascal[i-1][j] + pascal[i-1][j-1];
+						
+					}
+				}
+			}
+			for(int i = 0; i < filas; i++){
+				for(int j = 0; j < i+1; j++){
+					cout<<pascal[i][j]<<", ";
+				}
+				cout<<""<<endl;
+			}
 			break;
 		}
 		case 3:{
