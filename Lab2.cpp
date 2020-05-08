c8d643c15bd307e561b78f917bbda18a4c76bef1
agreyest <56709605+agreyest@users.noreply.github.com>
2020-05-08 16:12:26 -0600

    ejercicio 3 terminado

--- Lab2.cpp
+++ Lab2.cpp
@@ -1,4 +1,6 @@
-#include <iostream>
+#include<iostream>
+#include <stdlib.h>
+#include <time.h>
 using namespace std;
 
 int menu(){
@@ -8,7 +10,8 @@ int menu(){
 			<<"1.- Ejercicio 1"<<endl
 			<<"2.- Ejercicio 2"<<endl
 			<<"3.- Ejercicio 3"<<endl
-			<<"4.- Salir"<<endl;
+			<<"4.- Salir"<<endl
+			<<"Que opcion desea ver: ";
 			
 			cin>>retValue;
 			
@@ -69,7 +72,87 @@ int main(){
 			break;
 		}
 		case 3:{
+			int size;
+			cout<<endl<<"De que tamaño quiere el arreglo, debe ser numero par: ";
+			cin>>size;
+			while(size<1 || size % 2 != 0){
+				cout<<endl<<"intentelo de nuevo, el numero debe ser mayor a 0 y numero par: ";
+				cin>>size;
+			}
 			
+			int array[size];
+			srand(time(NULL));
+			
+			for(int i = 0; i < size; i++){
+				array[i] = (-50) + rand()%((51)-(-50));
+			}
+			
+			int J1[size/2];
+			int J2[size/2];
+			int selected[size];
+			for(int i = 0; i < size; i++){
+				selected[i]= -1;
+			}
+			int cont;
+			for(int i = 0; i < size/2; i++){
+				int numselected;
+				cout<<"J1, escoga un numero: ";
+				cin>>numselected;
+				
+				for(int j = 0; j < size; j++){
+					if( numselected == selected[j] ){
+						cout<<"J1 ese numero ya fue elegido, escoga otro numero: ";
+						cin>>numselected;
+						j=0;
+					}
+				}
+				selected[cont] = numselected;
+				cont++;
+				cout<<"J1 obtuvo: "<<array[numselected]<<endl;
+				J1[i] = array[numselected];
+				
+				
+				cout<<"J2, escoga un numero: ";
+				cin>>numselected;
+				
+				for(int j = 0; j < size; j++){
+					if(numselected == selected[j]){
+						cout<<"J2 ese numero ya fue elegido, escoga otro numero: ";
+						cin>>numselected;
+						j=0;
+					}
+				}
+				selected[cont] = numselected;
+				cont++;
+				cout<<"J2 obtuvo: "<<array[numselected]<<endl;
+				J2[i] = array[numselected];
+				
+				
+				int sumJ1=0;
+				int sumJ2=0;
+				for(int j = 0; j <= i; j++){
+					sumJ1 = sumJ1 + J1[j];
+					sumJ2 = sumJ2 + J2[j];
+				}
+				
+				if((i+1) == (size/2)){
+					cout<<"Final "<<" -  PTS J1: "<<sumJ1<<"  <->  PTS J2: "<<sumJ2<<endl;
+					if(sumJ1 > sumJ2){
+						cout<<"GANO J1"<<endl;
+					}else if(sumJ1 < sumJ2){
+						cout<<"GANO J2"<<endl;
+					}else if(sumJ1 == sumJ2){
+						cout<<"EMPATE"<<endl;
+					}
+					cout<<"Arreglo usado: [";
+					for(int j = 0; j < size; j++){
+						cout<<array[j]<<", ";
+					}
+					cout<<"]"<<endl;
+				}else{
+					cout<<"Ronda "<<i+1<<" -  PTS J1: "<<sumJ1<<"  <->  PTS J2: "<<sumJ2<<endl;
+				}
+			}//fin for del juego
 			break;
 		}
 		case 4:{
